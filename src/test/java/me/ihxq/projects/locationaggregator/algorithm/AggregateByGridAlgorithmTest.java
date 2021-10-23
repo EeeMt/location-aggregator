aea78e9fcbf72e05743a2c1832b50d509f119503
eeemt <hxq@live.com>
2021-10-23 14:10:54 +0800

    test: add test case

--- src/test/java/me/ihxq/projects/locationaggregator/algorithm/AggregateByGridAlgorithmTest.java
+++ src/test/java/me/ihxq/projects/locationaggregator/algorithm/AggregateByGridAlgorithmTest.java
@@ -15,22 +15,40 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AggregateByGridAlgorithmTest {
 
     @Test
+    void illegalPartitionSize() {
+        assertThrows(IllegalArgumentException.class, () -> AggregateByGridAlgorithm.of(2, 0));
+        assertThrows(IllegalArgumentException.class, () -> AggregateByGridAlgorithm.of(0, 2));
+        assertThrows(IllegalArgumentException.class, () -> AggregateByGridAlgorithm.of(0, 0));
+    }
+
+    @Test
+    void twoXOne() {
+        final List<SimpleLocation> locations = generateFourLocationDistributeBySquare();
+        final AggregateByGridAlgorithm algorithm = AggregateByGridAlgorithm.of(2, 1);
+        final Collection<AggregatedLocations<SquareScope, SimpleLocation>> result = algorithm.aggregate(locations);
+        assertEquals(2, result.size());
+        result.forEach(aggregatedLocation -> assertEquals(2, aggregatedLocation.getLocations().size()));
+    }
+
+    @Test
+    void oneXTwo() {
+        final List<SimpleLocation> locations = generateFourLocationDistributeBySquare();
+        final AggregateByGridAlgorithm algorithm = AggregateByGridAlgorithm.of(1, 2);
+        final Collection<AggregatedLocations<SquareScope, SimpleLocation>> result = algorithm.aggregate(locations);
+        assertEquals(2, result.size());
+        result.forEach(aggregatedLocation -> assertEquals(2, aggregatedLocation.getLocations().size()));
+    }
+
+    @Test
     void twoXTwo() {
-        final List<SimpleLocation> locations = Stream.of(
-                        SimpleLocation.of(0d, 0d),
-                        SimpleLocation.of(0d, 2d),
-                        SimpleLocation.of(2d, 0d),
-                        SimpleLocation.of(2d, 2d)
-                )
-                .collect(Collectors.toList());
+        final List<SimpleLocation> locations = generateFourLocationDistributeBySquare();
         final AggregateByGridAlgorithm algorithm = AggregateByGridAlgorithm.of(2, 2);
         final Collection<AggregatedLocations<SquareScope, SimpleLocation>> result = algorithm.aggregate(locations);
-        System.out.println(result);
         assertEquals(4, result.size());
         result.forEach(aggregatedLocation -> assertEquals(1, aggregatedLocation.getLocations().size()));
     }
@@ -52,8 +70,25 @@ class AggregateByGridAlgorithmTest {
 
         final AggregateByGridAlgorithm algorithm = AggregateByGridAlgorithm.of(3, 3);
         final Collection<AggregatedLocations<SquareScope, SimpleLocation>> result = algorithm.aggregate(locations);
-        System.out.println(result);
         assertEquals(9, result.size());
+        result.forEach(aggregatedLocation -> assertEquals(1, aggregatedLocation.getLocations().size()));
+    }
+
+    @Test
+    void twoXThree() {
+        final List<SimpleLocation> locations = Stream.of(
+                        SimpleLocation.of(0.0d, 0.0d),
+                        SimpleLocation.of(3.0d, 0.0d),
+                        SimpleLocation.of(0.5d, 1.5d),
+                        SimpleLocation.of(2.5d, 1.5d),
+                        SimpleLocation.of(0.0d, 3.0d),
+                        SimpleLocation.of(2.5d, 2.5d)
+                )
+                .collect(Collectors.toList());
+
+        final AggregateByGridAlgorithm algorithm = AggregateByGridAlgorithm.of(2, 3);
+        final Collection<AggregatedLocations<SquareScope, SimpleLocation>> result = algorithm.aggregate(locations);
+        assertEquals(6, result.size());
         result.forEach(aggregatedLocation -> assertEquals(1, aggregatedLocation.getLocations().size()));
     }
 
@@ -74,13 +109,23 @@ class AggregateByGridAlgorithmTest {
         final Instant start = Instant.now();
         final Collection<AggregatedLocations<SquareScope, SimpleLocation>> result = algorithm.aggregate(locations);
         final Instant end = Instant.now();
-        System.out.println(Duration.between(start, end).toMillis());
         assertEquals(9, result.size());
+        assertTrue(Duration.between(start, end).toMillis() < 100);
         final int sum = result.stream()
                 .map(AggregatedLocations::getLocations)
                 .mapToInt(Collection::size)
                 .sum();
         assertEquals(pointsNum, sum);
+    }
+
+    private List<SimpleLocation> generateFourLocationDistributeBySquare() {
+        return Stream.of(
+                        SimpleLocation.of(0d, 0d),
+                        SimpleLocation.of(0d, 2d),
+                        SimpleLocation.of(2d, 0d),
+                        SimpleLocation.of(2d, 2d)
+                )
+                .collect(Collectors.toList());
     }
 
 }
